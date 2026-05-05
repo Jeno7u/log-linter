99180c31dba586c0f0800042d00816c1facc2d26
Mironov Boris <lboris201@yandex.ru>
2026-05-05 20:29:28 +0300

    Moved more functions from analyzer to utils

--- analyzer/utils.go
+++ analyzer/utils.go
@@ -4,6 +4,8 @@ import (
 	"go/ast"
 	"go/token"
 	"strconv"
+
+	"golang.org/x/tools/go/analysis"
 )
 
 var sensitiveKeywords = []string{
@@ -44,4 +46,94 @@ func collectStringLiterals(expr ast.Expr) []string {
 	default:
 		return nil
 	}
+}
+
+// getString extracts an unquoted string from a BasicLit if possible.
+func getString(expr ast.Expr) (string, bool) {
+	lit, ok := expr.(*ast.BasicLit)
+	if !ok {
+		return "", false
+	}
+
+	if lit.Kind != token.STRING {
+		return "", false
+	}
+
+	s, err := strconv.Unquote(lit.Value)
+	if err != nil {
+		return "", false
+	}
+
+	return s, true
+}
+
+// getFuncName returns the simple function name for a CallExpr.
+func getFuncName(call *ast.CallExpr) string {
+	switch fun := call.Fun.(type) {
+	case *ast.SelectorExpr:
+		return fun.Sel.Name
+	case *ast.Ident:
+		return fun.Name
+	}
+	return ""
+}
+
+// checking is this call by "log/slog"
+func isSlogCall(pass *analysis.Pass, call *ast.CallExpr) bool {
+	sel, ok := call.Fun.(*ast.SelectorExpr)
+	if !ok {
+		return false
+	}
+
+	ident, ok := sel.X.(*ast.Ident)
+	if !ok {
+		return false
+	}
+
+	obj := pass.TypesInfo.Uses[ident]
+	if obj == nil {
+		return false
+	}
+
+	pkg := obj.Pkg()
+	if pkg == nil {
+		return false
+	}
+
+	return pkg.Path() == "log/slog"
+}
+
+// checking is this call by "go.uber.org/zap"
+func isZapCall(pass *analysis.Pass, call *ast.CallExpr) bool {
+	sel, ok := call.Fun.(*ast.SelectorExpr)
+	if !ok {
+		return false
+	}
+
+	innerCall, ok := sel.X.(*ast.CallExpr)
+	if !ok {
+		return false
+	}
+
+	innerSel, ok := innerCall.Fun.(*ast.SelectorExpr)
+	if !ok {
+		return false
+	}
+
+	ident, ok := innerSel.X.(*ast.Ident)
+	if !ok {
+		return false
+	}
+
+	obj := pass.TypesInfo.Uses[ident]
+	if obj == nil {
+		return false
+	}
+
+	pkg := obj.Pkg()
+	if pkg == nil {
+		return false
+	}
+
+	return pkg.Path() == "go.uber.org/zap"
 }
